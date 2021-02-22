34a50a21913c24cb9c2a2e30b05745778a1edbaa
Ricardo Taipe <ricardo.taipe@devsu.com>
2021-02-22 11:32:08 -0500

    Add mongodb collection

--- src/test/java/TicTacToeCollectionSpec.java
+++ src/test/java/TicTacToeCollectionSpec.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.UnknownHostException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicTacToeCollectionSpec {
+    TicTacToeCollection collection;
 
+    @BeforeEach
+    public void before() throws UnknownHostException {
+        collection = new TicTacToeCollection();
+    }
+
+    @Test
+    public void whenInstantiatedThenMongoHasDbNameTicTacToe() {
+        assertEquals("tic-tac-toe", collection.getMongoCollection().getDBCollection().getDB().getName());
+    }
+
+    @Test
+    public void whenInstantiatedThenMongoCollectionHasNameGame() throws UnknownHostException {
+        assertEquals("game", collection.getMongoCollection().getDBCollection().getName());
+    }
 }
